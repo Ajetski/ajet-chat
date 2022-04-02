de2bfbd77e3688c43a04fc2cda073d1361b2b5e4
Adam Jeniski <ajeniski4@gmail.com>
2022-04-02 16:40:22 -0400

    target correct back-end

--- web/App.svelte
+++ web/App.svelte
@@ -5,7 +5,7 @@
 	import { setClient, query } from 'svelte-apollo';
 
 	const client = new ApolloClient({
-		uri: 'http://localhost:4000/graphql',
+		uri: '/graphql',
 		cache: new InMemoryCache(),
 	});
 	setClient(client);
