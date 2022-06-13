0211a875a521a9585b90155b9ff9fa244c24abea
Adam Jeniski <Adam.Jeniski@ibm.com>
2022-06-13 08:45:28 -0400

    use adapter-auto

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,4 +1,4 @@
-import node from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: node()
+		adapter: adapter()
 	}
 };
 
