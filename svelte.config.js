7bdf12cd22c79f5bddeffdf0a9b4bf87bd732d4d
Adam Jeniski <ajeniski4@gmail.com>
2022-07-05 23:43:56 -0400

    fiddle with heroku build

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from './node_modules/@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
