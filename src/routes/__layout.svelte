955c14415395a53728bed42df3705f419ea7449a
Caleb Garrick <caleb.garrick@gmail.com>
2022-07-18 22:11:23 -0400

    AC-10 start right panel layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -22,6 +22,7 @@
 			},
 		};
 	};
+
 </script>
 
 <script lang="ts">
@@ -46,10 +47,66 @@
 	<div class="grid-center">
 		<slot />
 	</div>
-	<div class="grid-right">Text-Channel-Name</div>
+	<div class="grid-right">
+		<h2 style="margin: 0;">Text-Channel-Name</h2>
+		<div style="padding: 0 0 0 1rem;">
+			<div class="joinable">
+				<h3>Voice</h3>
+				<button type="button">Join</button>
+			</div>
+			{#each users as user}
+			<div class="vc-user">
+				<img class="profile-pic" src="https://u.cubeupload.com/Moonlight0619/pfp.png" alt="profile pic">
+				<h4 class="username">{user.username}</h4>
+				<button type="button">
+					<img src="" alt="">
+				</button>
+				<button type="button">
+					<img src="" alt="">
+				</button>
+			</div>
+			{/each}
+			<!-- <div class="joinable">
+				<h3>Stream</h3>
+				<button type="button">Join</button>
+			</div> -->
+		</div>
+	</div>
 </div>
 
 <style>
+	.joinable{
+		display: flex;
+		height: 3rem;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+	}
+	.joinable button{
+		height: 1.5rem;
+		width: 2.5rem;
+		border: none;
+		border-radius: 1rem;
+		background-color: var(--green-activity);
+		color: white;
+	}
+	.vc-user {
+		height: 2.5rem;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-between;
+		padding: 0 25% 0 0;
+	}
+	.profile-pic {
+		width: 2rem;
+		height: 2rem;
+		border-radius: 1rem;
+	}
+	.username {
+		width: 6rem;
+		overflow-x: hidden;
+	}
 	.grid-top {
 		grid-area: top;
 		border-bottom: 2px solid grey;
@@ -57,8 +114,7 @@
 	.grid-left {
 		grid-area: left;
 		background-color: var(--primary-100);
-		padding-left: 15px;
-		padding-top: 15px;
+		padding: 0 1rem 1rem;
 	}
 	.grid-center {
 		grid-area: center;
@@ -66,8 +122,7 @@
 	.grid-right {
 		grid-area: right;
 		background-color: var(--primary-100);
-		padding-left: 15px;
-		padding-top: 15px;
+		padding: 0 1rem 1rem;
 	}
 	.grid-container {
 		display: grid;
